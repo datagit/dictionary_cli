e88c778a2e27c873692de6224f25745b007a6a72
Dao Man Dat <z.dat.daoman@gree.net>
2018-08-07 17:55:14 +0700

    u2

--- DictionaryCli.php
+++ DictionaryCli.php
@@ -8,6 +8,11 @@
 
 class DictionaryCli
 {
+    public function a($params)
+    {
+        return $this->add($params);
+    }
+
     public function add($params)
     {
         //check validation here
@@ -18,6 +23,11 @@ class DictionaryCli
         );
         $d = new Dictionary();
         return $d->addWord($params);
+    }
+
+    public function e($params)
+    {
+        return $this->edit($params);
     }
 
     public function edit($params)
@@ -32,11 +42,21 @@ class DictionaryCli
         return $d->editWord($params);
     }
 
+    public function g($params)
+    {
+        return $this->get($params);
+    }
+
     public function get($params)
     {
         //check validation here
         $d = new Dictionary();
         return $d->getList($params['s']);
+    }
+
+    public function f($params)
+    {
+        return $this->find($params);
     }
 
     public function find($params)
--- cli.php
+++ cli.php
@@ -11,12 +11,30 @@ require_once 'Word.php';
 require_once 'Dictionary.php';
 require_once 'DictionaryCli.php';
 
-$options = getopt("m:w:e:f:s:");
 
-if (empty($options['m'])) {
-    echo sprintf("Please help me input:-m{Method} -w{Word} -E{Example}\n");
-    die;
+$method = readline("Please enter a your method [add, edit, get, find]:");
+
+if (in_array($method, array('add', 'a', 'edit', 'e'))) {
+    $word = readline("Please enter a your word:");
+    $example = readline("Please enter a your example:");
+    if ($method == 'edit' || $method == 'e') {
+        $favorite = readline("Please enter a your favorite[0,1]:");
+    }
+}elseif (in_array($method, array('get', 'g', 'find', 'f'))) {
+    $sort_type = readline("Please enter a your sort_type[1,2,3,4]:");
+    if($method == 'find' || $method == 'f') {
+        $word = readline("Please enter a your term:");
+    }
 }
+//$options = getopt("m:w:e:f:s:");
+$options = array(
+    'm' => $method,
+    'w' => $word,
+    'e' => $example,
+    'f' => $favorite,
+    's' => $sort_type,
+);
+
 $d = new DictionaryCli();
 $r = call_user_func_array(array($d, $options['m']), array($options));
 print_r($r);
