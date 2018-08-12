37447558623f755b87c4463f3e1c607841e1bfa1
Datagit <datagit@yahoo.com>
2018-08-12 16:21:39 +0700

    [Update]

--- cli.php
+++ cli.php
@@ -12,26 +12,27 @@ require_once 'Dictionary.php';
 require_once 'DictionaryCli.php';
 
 
-$method = readline("Please enter a your method [add, edit, get, find]:");
+//$method = readline("Please enter a your method [add, edit, get, find]:");
+//
+//if (in_array($method, array('add', 'a', 'edit', 'e'))) {
+//    $word = readline("Please enter a your word:");
+//    $example = readline("Please enter a your example:");
+//    if ($method == 'edit' || $method == 'e') {
+//        $favorite = readline("Please enter a your favorite[0,1]:");
+//    }
+//}elseif (in_array($method, array('get', 'g', 'find', 'f'))) {
+//    $sort_type = readline("Please enter a your sort_type[1,2,3,4]:");
+//    if($method == 'find' || $method == 'f') {
+//        $word = readline("Please enter a your term:");
+//    }
+//}
 
-if (in_array($method, array('add', 'a', 'edit', 'e'))) {
-    $word = readline("Please enter a your word:");
-    $example = readline("Please enter a your example:");
-    if ($method == 'edit' || $method == 'e') {
-        $favorite = readline("Please enter a your favorite[0,1]:");
-    }
-}elseif (in_array($method, array('get', 'g', 'find', 'f'))) {
-    $sort_type = readline("Please enter a your sort_type[1,2,3,4]:");
-    if($method == 'find' || $method == 'f') {
-        $word = readline("Please enter a your term:");
-    }
-}
 //$options = getopt("m:w:e:f:s:");
 $options = array(
-    'm' => $method,
-    'w' => $word,
-    'e' => $example,
-    'f' => $favorite,
+    'm' => $method = 'a',
+    'w' => $word = 'persuade',
+    'e' => $example = 'eeee',
+    'f' => $favorite = '1',
     's' => $sort_type,
 );
 
